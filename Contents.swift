6759ffe0cd05ba69c019860069ce48571573eb5e
thenaz <anaz7963@740s-pl-126.local>
2016-09-15 09:26:56 -0600

    Started using Swift and tried out my playground
    9/15

---
+++ Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var number = 12
+
+var name = "Ammar Nazir"
+
+var Name = "Otherwise known as the NAZ"
+
+
+
+
